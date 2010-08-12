a7b6b53f1d197a6f23bc51cd6d57e1196e62102d
Jared Sund <jsund@gmail.com>
2010-08-11 21:12:27 -0700

    test python file

--- client/src/testConn.py
+++ client/src/testConn.py
@@ -7,34 +7,44 @@
 ###
 ### Copyright (c) 2010 Brian L. Troutwine, Jared T. Sund, Cameron Kidd
 
-import harmonyConn 
+import harmonyConn
 from harmonyConn import *
 
 import lookupCoord
 from lookupCoord import *
 
 
-x = harmonyConn('sundhome.com', 1234)
-#x = harmonyConn('127.0.0.1', 1234)
+#x = harmonyConn('sundhome.com', 1234)
+x = harmonyConn('127.0.0.1', 1234)
 
 
 #test add star
 #(X,starID) = x.addStar(1,2,3)
+<<<<<<< HEAD
 print "Add Star", x.addStar(100,100,3)
 print "Add Star", x.addStar(200,200,3)
 print "Add Star", x.addStar(300,300,3)
+=======
+resp = (Conf, StarId) = x.addStar(1,2,3)
+print "Add Star", resp
+>>>>>>> de4562954846d260fcfabd9eb48b9068ab1d6647
 #print "Add Star", X, starID
 
 #test delete star
 #print "Del Star", x.delStar(0)
 
 #test add planet
+<<<<<<< HEAD
 print "Add Planet", x.addPlanet(0,1,2,30,4)
 print "Add Planet", x.addPlanet(0,5,6,30,8)
 print "Add Planet", x.addPlanet(0,1,2,60,4)
 print "Add Planet", x.addPlanet(1,1,2,30,4)
 print "Add Planet", x.addPlanet(1,5,6,30,8)
 print "Add Planet", x.addPlanet(1,5,6,60,8)
+=======
+print "Add Planet", x.addPlanet(StarId,1,2,3,4)
+#print "Add Planet", x.addPlanet(0,5,6,7,8)
+>>>>>>> de4562954846d260fcfabd9eb48b9068ab1d6647
 
 #test delete planet
 #print "Del Planet ", x.delPlanet(32, 43)
@@ -52,7 +62,7 @@ print "getUNI ", x.getUNI(0)
 #lookup(XCenter, YCenter, Radius, Angle)
 c = lookupCoord(0,0,10,270)
 
-#get the first arclength position based on the 
+#get the first arclength position based on the
     #planets startup angle
 #firstP = c.firstPosition
 #print firstP
